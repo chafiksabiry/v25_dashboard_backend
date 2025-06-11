456a541a4c45996c04de20c004eee711ea468a29
El Houcine Qara <elhoucine.qara@harx.ai>
2025-06-11 10:52:40 +0100

    Update dockerfile

---
+++ src/config/zoho.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    clientId: process.env.ZOHO_CLIENT_ID,
+    clientSecret: process.env.ZOHO_CLIENT_SECRET,
+    redirectUri: process.env.ZOHO_REDIRECT_URI,
+    authUrl: process.env.ZOHO_AUTH_URL,
+    tokenUrl: process.env.ZOHO_TOKEN_URL,
+    apiBaseUrl: process.env.ZOHO_API_URL,
+    scope: process.env.ZOHO_SCOPE,
+    salesIQPortal: process.env.ZOHO_SALESIQ_PORTAL
+}; 
