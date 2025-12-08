9dd2394db62dbd035eeeaee973c12ca26dcd80ac
El Houcine Qara <elhoucineqara114@gmail.com>
2025-12-08 19:14:31 +0100

    Update

--- src/app.js
+++ src/app.js
@@ -42,7 +42,7 @@ app.use(express.json());
   allowedHeaders: ['Content-Type', 'Authorization', 'x-channel']
 })); */
 app.use(cors({
-  origin: ['http://localhost:5180','https://v25-preprod.harx.ai', 'https://preprod-api-dash-calls.harx.ai','https://v25.harx.ai'],
+  origin: ['http://localhost:5180','https://v25-prod.harx.ai', 'https://prod-api-dash-calls.harx.ai','https://v25.harx.ai'],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
