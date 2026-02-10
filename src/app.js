ad9d8042c1dd6ecec59694d554868226100a88fe
elhoucineqara114 <elhoucineqara114@gmail.com>
2026-02-10 15:19:16 +0100

    update

--- src/app.js
+++ src/app.js
@@ -34,7 +34,8 @@ mongoose.connection.once('open', () => {
 const app = express();
 
 // Body parser
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 // Logger middleware (en développement)
 if (process.env.NODE_ENV === 'development') {
